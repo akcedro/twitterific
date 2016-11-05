d9879dd5ab2f404d25c5c32ab48d790a8f97be43
AIMEE CEDRO <AIMEE.CEDRO@GMAIL.COM>
2016-11-05 14:58:37 -0500

    Set up Tweet routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   get 'tweets/_form'
 
   devise_for :users
+
+  resources :tweets
+
+  root "tweets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
