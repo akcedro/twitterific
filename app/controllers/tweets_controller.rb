3d8d5a781983b2ab0ac9bbe65e026877ad7a4729
AIMEE CEDRO <AIMEE.CEDRO@GMAIL.COM>
2016-11-05 14:49:25 -0500

    Set up Tweet Controllers

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,16 @@
+class TweetsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def _form
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/show'
+
+  get 'tweets/edit'
+
+  get 'tweets/new'
+
+  get 'tweets/_form'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
