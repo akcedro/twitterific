0ca1252ebb525c688cbbe978bf8ebd48faab8854
AIMEE CEDRO <AIMEE.CEDRO@GMAIL.COM>
2016-11-07 19:12:50 -0600

    Set up root and routes * Root tweets index * All CRUD routes for tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
-
-  get 'tweets/show'
-
-  get 'tweets/edit'
-
-  get 'tweets/new'
-
-  get 'tweets/_form'
+  resources :tweets
 
   devise_for :users
 
-  resources :tweets
-
-  root "tweets#index"
+  root 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
